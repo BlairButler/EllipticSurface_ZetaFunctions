191a47729e3a90121016d5e3371c6aff432ddfa6
BlairButler <blairjb99@gmail.com>
2026-03-24 11:26:36 +1100

    Amend Example to be a Function

--- TestExample.m
+++ TestExample.m
@@ -1,12 +1,16 @@
 //Magma Code to (hopefully) compute the Zeta Function for the Elliptic Surface y^2 = x^3 + t^5 + t^-5
-time for j in [1..11] do;
+Gp:=GF(5);
+Gt<t>:=FunctionField(Gp);
+EE:=EllipticCurve([2*(t^8+14*t^4+1),4*t^2*(t^8+6*t^4+1)]); //From Kloosterman's Paper, has MW rank 15 over CC
 
+function ESZetaCoeffs(E,Gp,jj);
+coeffs:=[];
+for j in [1..jj] do;
 Gp:=GF(5^j);
 Gt<t>:=FunctionField(Gp);
-E:=EllipticCurve([2*(t^8+14*t^4+1),4*t^2*(t^8+6*t^4+1)]); //From Kloosterman's Paper, has MW rank 15 over CC
+E:=ChangeRing(EE,Gt);
 
 pts:=0;
-
 disTop:=Numerator(Discriminant(E));
 disBot:=Denominator(Discriminant(E));
 
@@ -14,7 +18,7 @@ Bads:=[Roots(f[1])[1][1] : f in Factorization(Numerator(Discriminant(E)))| Degre
 
 Goods:=[i : i in Gp | i notin Bads]; 
 Exclude(~Goods,0);
-time for i in Goods do;
+for i in Goods do;
 pts+:=#Specialization(E,i);
 end for;
 
@@ -25,5 +29,8 @@ S:=Scheme(k,f);
 pts+:=#Points(S)+1;
 end for;
 
-[j,pts];
+Include(~coeffs,[j,pts]);
 end for;
+return coeffs;
+end function;
+
